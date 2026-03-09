3bc90d8d093d3019b998b367ec98492691cddbf8
Ricardo MG da Silva <rephyrus0877@protonmail.com>
2026-03-09 12:14:25 +0000

    Add lowercase comparison

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -131,7 +131,9 @@ const Layout: React.FC<{
         const matchesAuthor =
           checkedAuthors.length === 0 ||
           //checkedAuthors.includes(item["schema:author"]?.["schema:name"]); // case api
-          checkedAuthors.some((author) => item.name?.includes(author)); // case local
+          checkedAuthors.some((author) =>
+            item.name?.toLowerCase().includes(author.toLowerCase())
+          ); // case local
 
         return matchesCatalog && matchesManufacturer && matchesAuthor;
       });
